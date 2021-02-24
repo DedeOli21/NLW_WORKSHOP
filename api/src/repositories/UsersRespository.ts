5db291c9cbff37f9a2c93fe76e36128319b17972
DedeOli21 <44575356+DedeOli21@users.noreply.github.com>
2021-02-24 18:39:28 -0300

    Initial Commit

---
+++ api/src/repositories/UsersRespository.ts
@@ -0,0 +1,7 @@
+import { EntityManager, EntityRepository, Repository } from "typeorm";
+import { User } from "../models/User";
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {}
+
+export { UsersRepository };
---
+++ api/src/server.ts
@@ -0,0 +1,13 @@
+import { app } from "./app";
+
+
+app.listen(3333, () => console.log("Server is running!"));
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ */
+
